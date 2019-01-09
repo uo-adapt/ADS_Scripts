0cab9aaa50171cba667fd41d1551a1bd74c241a4
Adam Pettitt <adampettitt89@gmail.com>
2019-01-09 12:52:07 -0800

    mc

--- dMRI/preproc/field_map.sh
+++ dMRI/preproc/field_map.sh
@@ -6,10 +6,9 @@ workspace="${data_dir}""${subid}""${wave}"
 
 cd "${workspace}"/dwi
 
-bet "${workspace}"fmap/"${subid}"_ses-wave2_magnitude2.nii.gz bet_magnitude1.nii.gz -f .6
+bet "${workspace}"fmap/"${subid}"_ses-wave2_magnitude2.nii.gz bet_magnitude1.nii.gz -f .5 -R
 
-fsl_prepare_fieldmap SIEMENS "${workspace}"fmap/"${subid}"_ses-wave2_phasediff.nii.gz bet_magnitude1.nii.gz fmap_rads 2.46
+fsl_prepare_fieldmap SIEMENS "${workspace}"fmap/"${subid}"_ses-wave2_phasediff.nii.gz bet_magnitude1.nii.gz fmap_rads 2.46 
 
+fugue -i "${subid}"_ses-wave2_dwi.nii.gz --dwell=0.00046484061 --loadfmap=fmap_rads -u dwi_corrected.nii --unwarpdir=y-
 
-
-fugue -i "${subid}"_ses-wave2_dwi.nii.gz --dwell=0.000400005 --loadfmap=fmap_rads -u dwi_corrected.nii
