1931b30d5f28adc5553019cb2d192c1bec28ac36
Adam Pettitt <adampettitt89@gmail.com>
2019-03-03 15:33:45 -0800

    mc

--- sMRI/wave2/job_map_annots_w2.sh
+++ sMRI/wave2/job_map_annots_w2.sh
@@ -30,34 +30,37 @@ rhannots=`cat rhannots.txt`
 lhlabels=`cat lhlabels.txt`
 rhlabels=`cat rhlabels.txt`
 
-pushd $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/label/ && mkdir fromannots && popd
-pushd $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/ && mkdir fromannots && popd
+pushd $SUBJECTS_DIR/$SUBID/label/ && mkdir fromannots && popd
+pushd $SUBJECTS_DIR/$SUBID/mri/ && mkdir fromannots && popd
+
+SUBID="${SUBID}"_ses-wave2.long.$SUBID
+echo $SUBJECTS_DIR/$SUBID
 
 for annot in $lhannots; do
-	mri_surf2surf --srcsubject fsaverage --trgsubject $SUBID --hemi lh --sval-annot $templatedir/"${annot}".annot --tval $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/label/"${annot}".annot
-	mri_annotation2label --subject ${SUBID} --hemi lh --annotation $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/label/"${annot}".annot --outdir $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/label/fromannots/
+	mri_surf2surf --srcsubject fsaverage --trgsubject $SUBID --hemi lh --sval-annot $templatedir/"${annot}".annot --tval $SUBJECTS_DIR/$SUBID/label/"${annot}".annot
+	mri_annotation2label --subject ${SUBID} --hemi lh --annotation $SUBJECTS_DIR/$SUBID/label/"${annot}".annot --outdir $SUBJECTS_DIR/$SUBID/label/fromannots/
 done
 
 for annot in $rhannots; do
-	mri_surf2surf --srcsubject fsaverage --trgsubject $SUBID --hemi rh --sval-annot $templatedir/"${annot}".annot --tval $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/label/"${annot}".annot
-	mri_annotation2label --subject ${SUBID} --hemi rh --annotation $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/label/"${annot}".annot --outdir $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/label/fromannots/
+	mri_surf2surf --srcsubject fsaverage --trgsubject $SUBID --hemi rh --sval-annot $templatedir/"${annot}".annot --tval $SUBJECTS_DIR/$SUBID/label/"${annot}".annot
+	mri_annotation2label --subject ${SUBID} --hemi rh --annotation $SUBJECTS_DIR/$SUBID/label/"${annot}".annot --outdir $SUBJECTS_DIR/$SUBID/label/fromannots/
 done
 
 for label in $lhlabels; do
-	mri_label2vol --label $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/label/fromannots/"${label}".label --subject $SUBID --hemi lh --identity --temp $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/orig --o $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}".nii.gz --proj frac 0 1 0.01
-	mri_binarize --dilate 1 --erode 1 --i $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}".nii.gz --o $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}"_bi.nii.gz --min 1
-	mris_calc -o $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}"_final.nii.gz $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}"_bi.nii.gz mul $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/lh.ribbon.mgz 
-	rm $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}"_bi.nii.gz
-	rm $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}".nii.gz
-	mv $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}"_final.nii.gz $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}".nii.gz
+	mri_label2vol --label $SUBJECTS_DIR/$SUBID/label/fromannots/"${label}".label --subject $SUBID --hemi lh --identity --temp $SUBJECTS_DIR/$SUBID/mri/orig --o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz --proj frac 0 1 0.01
+	mri_binarize --dilate 1 --erode 1 --i $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz --o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz --min 1
+	mris_calc -o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_final.nii.gz $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz mul $SUBJECTS_DIR/$SUBID/mri/lh.ribbon.mgz 
+	rm $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz
+	rm $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz
+	mv $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_final.nii.gz $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz
  done
 
  for label in $rhlabels; do
-	mri_label2vol --label $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/label/fromannots/"${label}".label --subject $SUBID --hemi rh --identity --temp $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/orig --o $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}".nii.gz --proj frac 0 1 0.01
-	mri_binarize --dilate 1 --erode 1 --i $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}".nii.gz --o $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}"_bi.nii.gz --min 1
-	mris_calc -o $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}"_final.nii.gz $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}"_bi.nii.gz mul $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/rh.ribbon.mgz 
-	rm $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}"_bi.nii.gz
-	rm $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}".nii.gz
-	mv $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}"_final.nii.gz $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$SUBID/mri/fromannots/"${label}".nii.gz
+	mri_label2vol --label $SUBJECTS_DIR/$SUBID/label/fromannots/"${label}".label --subject $SUBID --hemi rh --identity --temp $SUBJECTS_DIR/$SUBID/mri/orig --o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz --proj frac 0 1 0.01
+	mri_binarize --dilate 1 --erode 1 --i $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz --o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz --min 1
+	mris_calc -o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_final.nii.gz $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz mul $SUBJECTS_DIR/$SUBID/mri/rh.ribbon.mgz 
+	rm $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz
+	rm $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz
+	mv $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_final.nii.gz $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz
  done
 
