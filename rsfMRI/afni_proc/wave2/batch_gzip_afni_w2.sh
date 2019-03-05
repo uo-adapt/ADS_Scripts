144d2df9ca85154727c1b13c0f25832f4f10eda8
Adam Pettitt <adampettitt89@gmail.com>
2019-03-05 08:06:44 -0800

    mc

--- rsfMRI/afni_proc/wave2/batch_gzip_afni_w2.sh
+++ rsfMRI/afni_proc/wave2/batch_gzip_afni_w2.sh
@@ -12,6 +12,6 @@ STUDY=/projects/adapt_lab/shared/ADS
 SUBJLIST=`cat sub_test.txt`
 
 for SUBJ in $SUBJLIST
- do sbatch --export SUBID=${SUBJ} --job-name gzipit --partition=short --mem-per-cpu=3G --cpus-per-task=1 -o "${STUDY}"/Scripts/rsfMRI/afni_proc/wave_2/output/"${SUBJ}"_gzipit_output_w2.txt -e "${STUDY}"/Scripts/rsfMRI/afni_proc/wave_2/output/"${SUBJ}"_gzipit_error_w2.txt job_gzip_afni_w2.sh
+ do sbatch --export SUBID=${SUBJ} --job-name gzipit --partition=short --mem-per-cpu=3G --cpus-per-task=1 -o "${STUDY}"/Scripts/rsfMRI/afni_proc/wave2/output/"${SUBJ}"_gzipit_output_w2.txt -e "${STUDY}"/Scripts/rsfMRI/afni_proc/wave2/output/"${SUBJ}"_gzipit_error_w2.txt job_gzip_afni_w2.sh
 done
 
