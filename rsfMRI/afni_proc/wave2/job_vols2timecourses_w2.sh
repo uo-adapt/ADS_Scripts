7fde84a7c5279cf871f9767b63d91b9461b4bbe4
Adam Pettitt <adampettitt89@gmail.com>
2019-03-05 08:17:48 -0800

    mc

--- rsfMRI/afni_proc/wave2/job_vols2timecourses_w2.sh
+++ rsfMRI/afni_proc/wave2/job_vols2timecourses_w2.sh
@@ -34,22 +34,22 @@ pushd $rsfMRIdir/$SUBID/"${SUBID}".results/ && mkdir alignedrois && popd
 pushd $rsfMRIdir/$SUBID/"${SUBID}".results/ && mkdir timecourses && popd
 
 for label in $lhlabels; do
- 3dAllineate -source $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz -master $rsfMRIdir/"${SUBID}"/"${SUBID}".results/*r01.volreg.nii.gz -final NN -1Dparam_apply '1D: 12@0'\' -prefix $rsfMRIdir/"${SUBID}"/"${SUBID}".results/alignedrois/"${label}"_aligned.nii.gz 
+ 3dAllineate -source $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$subj/mri/fromannots/"${label}".nii.gz -master $rsfMRIdir/"${SUBID}"/"${SUBID}".results/*r01.volreg.nii.gz -final NN -1Dparam_apply '1D: 12@0'\' -prefix $rsfMRIdir/"${SUBID}"/"${SUBID}".results/alignedrois/"${label}"_aligned.nii.gz 
 
  3dmaskave -quiet -mask $rsfMRIdir/"${SUBID}"/"${SUBID}".results/alignedrois/"${label}"_aligned.nii.gz $rsfMRIdir/"${SUBID}"/"${SUBID}".results/errts."${SUBID}".fanaticor.nii.gz > $rsfMRIdir/"${SUBID}"/"${SUBID}".results/timecourses/"${SUBID}"_"${label}".txt
 
  rm $rsfMRIdir/"${SUBID}"/"${SUBID}".results/alignedrois/"${label}"_aligned.nii.gz 
-# rm $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz
+# rm $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$subj/mri/fromannots/"${label}".nii.gz
 
 done
 
 for label in $rhlabels; do
- 3dAllineate -source $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz -master $rsfMRIdir/"${SUBID}"/"${SUBID}".results/*r01.volreg.nii.gz -final NN -1Dparam_apply '1D: 12@0'\' -prefix $rsfMRIdir/"${SUBID}"/"${SUBID}".results/alignedrois/"${label}"_aligned.nii.gz 
+ 3dAllineate -source $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$subj/mri/fromannots/"${label}".nii.gz -master $rsfMRIdir/"${SUBID}"/"${SUBID}".results/*r01.volreg.nii.gz -final NN -1Dparam_apply '1D: 12@0'\' -prefix $rsfMRIdir/"${SUBID}"/"${SUBID}".results/alignedrois/"${label}"_aligned.nii.gz 
 
  3dmaskave -quiet -mask $rsfMRIdir/"${SUBID}"/"${SUBID}".results/alignedrois/"${label}"_aligned.nii.gz $rsfMRIdir/"${SUBID}"/"${SUBID}".results/errts."${SUBID}".fanaticor.nii.gz > $rsfMRIdir/"${SUBID}"/"${SUBID}".results/timecourses/"${SUBID}"_"${label}".txt
 
  rm $rsfMRIdir/"${SUBID}"/"${SUBID}".results/alignedrois/"${label}"_aligned.nii.gz 
- #rm $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz
+ #rm $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$subj/mri/fromannots/"${label}".nii.gz
 
 done
 
@@ -62,9 +62,9 @@ aparc2009rois=`cat aparc2009rois.txt`
 asegrois=`cat asegrois.txt`
 popd
 
-3dAllineate -source $SUBJECTS_DIR/$SUBID/SUMA/aparc.a2009s+aseg.nii.gz -master $rsfMRIdir/"${SUBID}"/"${SUBID}".results/*r01.volreg.nii.gz -final NN -1Dparam_apply '1D: 12@0'\' -prefix $rsfMRIdir/"${SUBID}"/"${SUBID}".results/aparc.a2009s+aseg_aligned.nii.gz 
+3dAllineate -source $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$subj/SUMA/aparc.a2009s+aseg.nii.gz -master $rsfMRIdir/"${SUBID}"/"${SUBID}".results/*r01.volreg.nii.gz -final NN -1Dparam_apply '1D: 12@0'\' -prefix $rsfMRIdir/"${SUBID}"/"${SUBID}".results/aparc.a2009s+aseg_aligned.nii.gz 
 
-3dAllineate -source $SUBJECTS_DIR/$SUBID/SUMA/aparc+aseg.nii.gz -master $rsfMRIdir/"${SUBID}"/"${SUBID}".results/*r01.volreg.nii.gz -final NN -1Dparam_apply '1D: 12@0'\' -prefix $rsfMRIdir/"${SUBID}"/"${SUBID}".results/aparc+aseg_aligned.nii.gz 
+3dAllineate -source $SUBJECTS_DIR/"${SUBID}"_ses-wave2.long.$subj/SUMA/aparc+aseg.nii.gz -master $rsfMRIdir/"${SUBID}"/"${SUBID}".results/*r01.volreg.nii.gz -final NN -1Dparam_apply '1D: 12@0'\' -prefix $rsfMRIdir/"${SUBID}"/"${SUBID}".results/aparc+aseg_aligned.nii.gz 
 
 for aparcroi in $aparcrois; do
 	3dmaskave -quiet -mrange "${aparcroi}" "${aparcroi}" -mask $rsfMRIdir/"${SUBID}"/"${SUBID}".results/aparc+aseg_aligned.nii.gz $rsfMRIdir/"${SUBID}"/"${SUBID}".results/errts."${SUBID}".fanaticor.nii.gz > $rsfMRIdir/"${SUBID}"/"${SUBID}".results/timecourses/"${SUBID}"_aparc_"${aparcroi}".txt
