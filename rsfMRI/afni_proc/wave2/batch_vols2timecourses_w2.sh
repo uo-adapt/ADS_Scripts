c1e38cbcbbefa0511352c4ab44486137ebe7fc2d
Adam Pettitt <adampettitt89@gmail.com>
2019-03-05 08:11:31 -0800

    mc

--- rsfMRI/afni_proc/wave2/batch_vols2timecourses_w2.sh
+++ rsfMRI/afni_proc/wave2/batch_vols2timecourses_w2.sh
@@ -14,5 +14,5 @@ SUBJLIST=`cat sub_test.txt`
 
 
 for SUBJ in $SUBJLIST
- do sbatch --export SUBID=${SUBJ} --job-name vols2timecourses_w2 --partition=short --mem-per-cpu=8G --cpus-per-task=1 -o "${STUDY}"/Scripts/rsfMRI/afni_proc/wave_2/output/"${SUBJ}"_vols2timecourses_w2output.txt -e "${STUDY}"/Scripts/rsfMRI/afni_proc/wave_2/output/"${SUBJ}"_vols2timecourses__w2error.txt job_vols2timecourses_w2.sh
+ do sbatch --export SUBID=${SUBJ} --job-name vols2timecourses_w2 --partition=short --mem-per-cpu=8G --cpus-per-task=1 -o "${STUDY}"/Scripts/rsfMRI/afni_proc/wave2/output/"${SUBJ}"_vols2timecourses_w2output.txt -e "${STUDY}"/Scripts/rsfMRI/afni_proc/wave2/output/"${SUBJ}"_vols2timecourses__w2error.txt job_vols2timecourses_w2.sh
 done
