21feb7d80ef53a01ed881593f2a255e76fc1d341
pettitta <adampettitt89@gmail.com>
2019-05-31 12:56:25 -0700

    mc

--- sMRI/multiAtlasTT/src/maTT2_applyGCS.sh
+++ sMRI/multiAtlasTT/src/maTT2_applyGCS.sh
@@ -116,7 +116,7 @@ fi
 
 # make full path, add forward slash too
 inputFSDir=$(readlink -f ${inputFSDir})/
-echo $inputFSDir
+echo testing $inputFSDir
 outputDir=${outputDir}/
 
 # check existence of FS directory
