6ad7a1ea5222a8d7f814f435399a53cbfb831c68
Adam Pettitt <adampettitt89@gmail.com>
2019-05-31 18:27:06 -0700

    mc

--- sMRI/multiAtlasTT/submit.sh
+++ sMRI/multiAtlasTT/submit.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=maTT         ### Job Name
 #SBATCH --output=Hi.out         ### File in which to store job output
 #SBATCH --error=Hi.err          ### File in which to store job error messages
-#SBATCH --time=0-04:01:00       ### Wall clock time limit in Days-HH:MM:SS
+#SBATCH --time=0-08:01:00       ### Wall clock time limit in Days-HH:MM:SS
 #SBATCH --mem-per-cpu=15G
 #SBATCH --nodes=1               ### Number of nodes needed for the job
 #SBATCH --ntasks-per-node=1     ### Number of tasks to be launched per Node
