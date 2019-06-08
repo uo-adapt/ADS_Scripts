2043eab90cdbdf4a293f5a2fa87f508002430fce
Adam Pettitt <adampettitt89@gmail.com>
2019-06-08 10:54:45 -0700

    cm

--- sMRI/job_map_annots.sh
+++ sMRI/job_map_annots.sh
@@ -47,7 +47,7 @@ for annot in $rhannots; do
 done
 
 for label in $lhlabels; do
-	mri_label2vol --label $SUBJECTS_DIR/$SUBID/label/fromannots/"${label}".label --subject $SUBID --hemi lh --identity --temp $SUBJECTS_DIR/$SUBID/mri/orig --o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz --proj frac 0 1 0.01
+	mri_label2vol --label $SUBJECTS_DIR/$SUBID/label/fromannots/"${label}".label --subject $SUBID --hemi lh --identity --temp $SUBJECTS_DIR/$SUBID/mri/orig.mgz --o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz --proj frac 0 1 0.01
 	mri_binarize --dilate 1 --erode 1 --i $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz --o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz --min 1
 	mris_calc -o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_final.nii.gz $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz mul $SUBJECTS_DIR/$SUBID/mri/lh.ribbon.mgz 
 	rm $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz
@@ -56,7 +56,7 @@ for label in $lhlabels; do
  done
 
  for label in $rhlabels; do
-	mri_label2vol --label $SUBJECTS_DIR/$SUBID/label/fromannots/"${label}".label --subject $SUBID --hemi rh --identity --temp $SUBJECTS_DIR/$SUBID/mri/orig --o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz --proj frac 0 1 0.01
+	mri_label2vol --label $SUBJECTS_DIR/$SUBID/label/fromannots/"${label}".label --subject $SUBID --hemi rh --identity --temp $SUBJECTS_DIR/$SUBID/mri/orig.mgz --o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz --proj frac 0 1 0.01
 	mri_binarize --dilate 1 --erode 1 --i $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}".nii.gz --o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz --min 1
 	mris_calc -o $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_final.nii.gz $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz mul $SUBJECTS_DIR/$SUBID/mri/rh.ribbon.mgz 
 	rm $SUBJECTS_DIR/$SUBID/mri/fromannots/"${label}"_bi.nii.gz
