e5732616ee6e498bc98ebc6b0add8eae84e41b11
pettitta <adampettitt89@gmail.com>
2020-02-04 15:26:32 -0800

    adding in old stuff

---
+++ rsfMRI/xcpEngine/xcp_rest.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+
+module load singularity
+module load afni
+module load ants
+module load fsl
+module load c3d
+module load R
+module load python3
+
+cd /projects/adapt_lab/shared/ADS/Scripts/rsfMRI/xcpEngine
+
+export XCPEDIR=/projects/adapt_lab/shared/ADS/Scripts/rsfMRI/xcpEngine
+SIMG=/projects/adapt_lab/shared/containers/xcpEngine.simg
+HOME=/projects/adapt_lab/shared/ADS
+
+singularity run -B ${DATA_ROOT}:${HOME} $SIMG \
+   -d ${HOME}/Scripts/rsfMRI/xcpEngine/fc-ICA-AROMA_201903131537_1.dsn \
+   -c "${TEMP_COHORT}",${ses} \
+   -o ${HOME}/data/BIDS_data/derivatives/xcpEngine/data \
+   -t 2 \
+   -i \$TMPDIR
+
