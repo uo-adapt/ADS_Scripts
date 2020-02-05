603870f8a2f4d7f35b3d019a78fcba6714eabf23
Adam Pettitt <adampettitt89@gmail.com>
2020-02-04 19:16:53 -0800

    mc

--- rsfMRI/xcpEngine/xcp_rest.sh
+++ rsfMRI/xcpEngine/xcp_rest.sh
@@ -5,7 +5,6 @@ module load singularity
 module load afni
 module load ants
 module load fsl
-module load c3d
 module load R
 module load python3
 
@@ -15,7 +14,7 @@ export XCPEDIR=/projects/adapt_lab/shared/ADS/Scripts/rsfMRI/xcpEngine
 SIMG=/projects/adapt_lab/shared/containers/xcpEngine.simg
 HOME=/projects/adapt_lab/shared/ADS
 
-singularity run -B ${DATA_ROOT}:${HOME} $SIMG \
+./xcpEngine\
    -d ${HOME}/Scripts/rsfMRI/xcpEngine/fc-ICA-AROMA_202002041521.dsn \
    -c "${TEMP_COHORT}",${ses} \
    -o ${HOME}/data/BIDS_data/derivatives/xcpEngine/data \
