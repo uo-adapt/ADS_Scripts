7d04a4dd23168fd97e26cbd95b3dcfc23c14cb05
Adam Pettitt <adampettitt89@gmail.com>
2020-02-06 08:24:27 -0800

    mc

---
+++ sMRI/multiAtlasTT/batch_map_annots.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This batch file calls on your subject
+# list (named subject_list.txt). And 
+# runs the job_SUMA.sh file for 
+# each subject. It saves the ouput
+# and error files in their specified
+# directories.
+#
+# Set your study
+STUDY=/projects/adapt_lab/shared/ADS
+
+# Set subject list
+SUBJLIST=`cat sub_test.txt`
+#SUBJLIST=`cat test.txt`
+
+# 
+for SUBJ in $SUBJLIST
+do
+sbatch --export SUBID=${SUBJ} --job-name mapANNOTs_"${SUBJ}" --partition=short --time=3:00:00 -A adapt_lab --mem-per-cpu=10G --cpus-per-task=1 -o "${STUDY}"/Scripts/sMRI/multiAtlasTT/output/"${SUBJ}"_multiAtlasTT_output.txt -e "${STUDY}"/Scripts/sMRI/multiAtlasTT/output/"${SUBJ}"_multiAtlasTT_error.txt job_multiAtlasTT.sh
+done
